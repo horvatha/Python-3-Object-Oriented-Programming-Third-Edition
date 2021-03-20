e98d139800d353774da236a763d2cfe83e213da5
Árpád Horváth <horvath.arpad.szfvar@gmail.com>
2021-03-20 17:59:35 +0100

    score_tally example call added

--- Chapter09/score_tally.py
+++ Chapter09/score_tally.py
@@ -1,5 +1,25 @@
+from icecream import ic
+
+
 def tally():
     score = 0
     while True:
         increment = yield score
+        if increment is None:
+            increment = 0
         score += increment
+
+
+if __name__ == '__main__':
+    enthroners = tally()
+    sharks = tally()
+    ic(next(enthroners))
+    ic(next(sharks))
+    ic(next(enthroners))
+    ic(next(sharks))
+    ic(enthroners.send(20))
+    ic(sharks.send(10))
+    ic(enthroners.send(15))
+    ic(sharks.send(20))
+    ic(next(enthroners))
+    ic(next(sharks))
